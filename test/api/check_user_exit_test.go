67f5ec0f95f3eea383d5c995442c2870eb42cfbb
evanxu <evanxu@didiglobal.com>
2022-10-09 23:25:11 +0800

    add_test

--- test/api/check_user_exit_test.go
+++ test/api/check_user_exit_test.go
@@ -5,6 +5,7 @@ import (
 	. "gopkg.in/check.v1"
 	"otherpay-test/common"
 	"testing"
+	"encoding/json"
 )
 
 
@@ -24,6 +25,16 @@ type CheckUserExistReq struct {
 	Addr string `json:"addr"`
 }
 
+type CheckUserExistResp struct {
+	Code int `json:"code"`
+	Message string `json:"message"`
+	Data CheckUserExistRespData `json:"data"`
+}
+
+type CheckUserExistRespData struct {
+	IsExist bool `json:"is_exist"`
+}
+
 func (s *CheckUserExist) TestCheckUserExistCase00(goCheck *C) {
 	//
 	privateHex := "ae78c8b502571dba876742437f8bc78b689cf8518356c0921393d89caaf284ce"
@@ -32,8 +43,11 @@ func (s *CheckUserExist) TestCheckUserExistCase00(goCheck *C) {
 	req := CheckUserExistReq {
 		Addr: "test_not_in_db",
 	}
-	resp, err := common.DoPost(urlCheckUserExist, common.ConvToJSON(req))
+	respStr, err := common.DoPost(urlCheckUserExist, common.ConvToJSON(req))
+	var resp CheckUserExistResp
 	goCheck.Assert(err, IsNil)
-	fmt.Println(string(resp))
+	err = json.Unmarshal(respStr, &resp)
+	goCheck.Assert(resp.Code, Equals, 0)
+	goCheck.Assert(resp.Data.IsExist, Equals, false)
 }
 
