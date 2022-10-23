a86196c81c9e410ec1afdcdfa0eae117a857efa9
evanxu <evanxu@didiglobal.com>
2022-10-23 22:07:01 +0800

    fix

--- test/index3/login_or_register_test.go
+++ test/index3/login_or_register_test.go
@@ -3,18 +3,16 @@ package index3
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/tendermint/tendermint/libs/bytes"
 	. "gopkg.in/check.v1"
 	"otherpay-test/client"
 	"otherpay-test/common"
 	"testing"
-	"github.com/tendermint/tendermint/libs/bytes"
 
 	"time"
 )
 
-
 func Test(t *testing.T) { TestingT(t) }
-
 
 type LoginOrRegister struct {
 }
@@ -27,8 +25,8 @@ var (
 
 type CheckInfo struct {
 	Address string `json:"address" example:"0x82198867e32e4405f0d4ff46a5a1d214c9b1d474"`
-	Msg string  `json:"msg" example:"I am signing my one-time nonce: ABCDEF"`
-	Sign string `json:"sign" example:"0xa9481518a8d279a5936735b50f1bda60f2998e745acfd406a6d6dd7e25786465119fb2c1bdc58fcc7987f996c8e787be094acbbeda2d05ea3507a129e9c26bb21c"`
+	Msg     string `json:"msg" example:"I am signing my one-time nonce: ABCDEF"`
+	Sign    string `json:"sign" example:"0xa9481518a8d279a5936735b50f1bda60f2998e745acfd406a6d6dd7e25786465119fb2c1bdc58fcc7987f996c8e787be094acbbeda2d05ea3507a129e9c26bb21c"`
 }
 
 type RequestLoginOrRegister struct {
@@ -47,18 +45,13 @@ type Response struct {
 	Data    interface{}    `json:"data,omitempty"`
 }
 
-
-
-
-
-
-
 func (s *LoginOrRegister) TestRegisterCase00(goCheck *C) {
 	//参数合法，可以注册成功
 	privateHex := "ae78c8b502571dba876742437f8bc78b689cf8518356c0921393d89caaf284ce"
 	msg := "I am registing for index3 "
 	addr, sign := common.GetSignNew(privateHex, msg)
-	sql := fmt.Sprintf("delete from mirror_user where address = \"%s\"", addr)
+	//sql := fmt.Sprintf("delete from mirror_user where address = \"%s\"", addr)
+	sql := fmt.Sprintf("delete from mirror_user where address = \"%s\"", "2")
 	_, err := client.MysqlClientIndex3().Exec(sql)
 	goCheck.Assert(err, IsNil)
 	req := RequestLoginOrRegister{
@@ -75,8 +68,46 @@ func (s *LoginOrRegister) TestRegisterCase00(goCheck *C) {
 	err = json.Unmarshal(respStr, &resp)
 	goCheck.Assert(err, IsNil)
 	goCheck.Assert(resp.Code, Equals, uint32(0))
-	respToken := resp.Data.(ResponseToken)
-	goCheck.Assert(len(respToken.Token), Not(Equals), 0)
+	respToken := resp.Data.(map[string]interface{})
+
+	goCheck.Assert(len(respToken["token"].(string)), Not(Equals), 0)
+	sql = fmt.Sprintf("select address from mirror_user where address = \"%s\"", addr)
+	rows, err := client.MysqlClientIndex3().Query(sql)
+	var address string
+	for rows.Next() {
+		rows.Scan(&address)
+		break
+	}
+	goCheck.Assert(address, Equals, addr)
+	fmt.Println(string(respStr))
+}
+
+func (s *LoginOrRegister) TestRegisterCase01(goCheck *C) {
+	//参数不合法，校验失败
+	privateHex := "ae78c8b502571dba876742437f8bc78b689cf8518356c0921393d89caaf284ce"
+	msg := "I am registing for index3 "
+	addr, sign := common.GetSignNew(privateHex, msg)
+	//sql := fmt.Sprintf("delete from mirror_user where address = \"%s\"", addr)
+	sql := fmt.Sprintf("delete from mirror_user where address = \"%s\"", "2")
+	_, err := client.MysqlClientIndex3().Exec(sql)
+	goCheck.Assert(err, IsNil)
+	req := RequestLoginOrRegister{
+		CheckInfo: CheckInfo{
+			Address: addr,
+			Msg:     msg + " test",
+			Sign:    sign,
+		},
+		LoginTime: time.Now().Unix(),
+	}
+	respStr, err := common.DoPost(urlLoginOrRegister, common.ConvToJSON(req))
+	goCheck.Assert(err, IsNil)
+	var resp Response
+	err = json.Unmarshal(respStr, &resp)
+	goCheck.Assert(err, IsNil)
+	goCheck.Assert(resp.Code, Equals, uint32(0))
+	respToken := resp.Data.(map[string]interface{})
+
+	goCheck.Assert(len(respToken["token"].(string)), Not(Equals), 0)
 	sql = fmt.Sprintf("select address from mirror_user where address = \"%s\"", addr)
 	rows, err := client.MysqlClientIndex3().Query(sql)
 	var address string
@@ -106,5 +137,3 @@ func (s *LoginOrRegister) TestRegisterCase00(goCheck *C) {
 //	err = json.Unmarshal(respStr, &resp)
 //	goCheck.Assert(resp.Code, Not(Equals), 0)
 //}
-
-
